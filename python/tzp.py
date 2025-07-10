6fda6469df634a0cad5ce4862b1f14ba99a4c8b6
jo-devnull <jorge.devnull@gmail.com>
2025-07-10 10:50:51 -0300

    update modpack

--- python/tzp.py
+++ python/tzp.py
@@ -9,33 +9,48 @@ from pathlib import Path
 input = Path(sys.argv[1]).resolve()
 textures = input / "assets/minecraft/optifine/random/entity/zombie"
 
-output = Path(os.getcwd()) / "TZP-MMV-Patch"
-data = output / "data/moremobvariants/variants/zombie/"
-assets = output / "assets/moremobvariants/textures/entity/zombie/"
+output = Path(os.getcwd()) / "TZP-Reduced"
+assets = output / "assets/minecraft/optifine/random/entity/zombie"
 
 if output.exists():
   shutil.rmtree(output)
 
 output.mkdir()
-data.mkdir(parents=True)
 assets.mkdir(parents=True)
+
+for file in [
+  "assets/minecraft/optifine/emissive.properties",
+  "assets/minecraft/sounds.json",
+  "pack.png"
+]:
+  shutil.copyfile(input / file, output / file)
+
+shutil.copytree(
+  input / "assets/minecraft/textures",
+  output / "assets/minecraft/textures"
+)
+
+shutil.copytree(
+  input / "assets/minecraft/sounds",
+  output / "assets/minecraft/sounds"
+)
 
 selected = random.sample([
   file for file in textures.glob("zombie*.png")
-  if not file.name.endswith("_e.png")
+  if not file.name.endswith("_e.png") and not file.name.endswith("-e.png")
 ], 128)
 
 for i, file in enumerate(selected):
-  data_file = data / f"zombie{i}.json"
-  asset_file = assets / f"zombie{i}.png"
+  texture = assets / f"zombie{i}.png"
+  emissive = file.with_name(file.name.replace(".png", "_e.png"))
 
-  shutil.copyfile(file, asset_file)
-  data_file.write_text('{ "weight": 1 }')
+  shutil.copyfile(file, texture)
+  shutil.copyfile(emissive, assets / f"zombie{i}_e.png")
 
 (output / "pack.mcmeta").write_text('''{
   "pack": {
       "pack_format": 15,
-      "description": "Tissou's Zombie Pack - Patched to work with MoreMobVariants."
+      "description": "Tissou's Zombie Pack - Reduced."
   }
 }''')
 
