576d53352784479caef2e6658744452665dd37c4
jo-devnull <jorge.devnull@gmail.com>
2026-05-16 16:26:25 -0300

    update modpack

--- data/@lib/api/types/types.ts
+++
@@ -1,7 +0,0 @@
-export type Int = number;
-export type Float = number;
-
-// deno-lint-ignore ban-types
-export type Resource = {};
-export type ResourceType = "assets" | "data" | "*";
-export type ResourceLocation = `${string}:${string}`;
--- data/main.ts
+++
@@ -1,5 +0,0 @@
-import { build } from "./@lib/build.ts"
-
-import "./toolworking/casting.ts"
-
-build(Deno.args[0]);
